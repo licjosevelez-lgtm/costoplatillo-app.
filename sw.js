cb12de681b77b0692a2a78cd3122c9bbc5e15c43
licjosevelez-lgtm <lic.jose.velez@hotmail.com>
2025-08-14 08:53:17 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+
+const CACHE_NAME = 'costo-platillo-v2';
+const OFFLINE_URL = 'offline.html';
+const PRECACHE_URLS = [
+  './',
+  'index.html',
+  'index.css',
+  'index.tsx',
+  'manifest.json',
+  OFFLINE_URL
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(PRECACHE_URLS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+        .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => caches.match(OFFLINE_URL))
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then(response => response || fetch(event.request))
+        );
+    }
+});
